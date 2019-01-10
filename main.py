c706ff0a9e019cba7eb7245a45364bb5b4e186dd
Apoorv Goel <apoorvgoel_bt2k16@dtu.ac.in>
2019-01-11 00:00:15 +0530

    Final commit

--- main.py
+++ main.py
@@ -4,23 +4,43 @@ from matplotlib import pyplot as plt
 
 import pallier
 
-fname = "Lenna.png"
+ekey, dkey = pallier.generate_key(5)
+
+def new_encrypt(num):
+	global ekey
+	e = pallier.encrypt(num, ekey)
+	print e
+	return int(e)
+
+def new_decrypt(num):
+	global ekey
+	global dkey
+	return int(pallier.decrypt(num, dkey, ekey))
+
+fname = "hat.png"
 
 img = cv2.imread(fname)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#cv2.imshow("Gray Lenna", img)
+height, width = img.shape
 
 print "Loaded %s in grayscale, encrypting image.." % fname
 
-encrypt_vec = np.vectorize(pallier.encrypt)
-img_enc = encrypt_vec(img)
+img_enc = np.zeros((height, width), dtype=np.uint64)
+
+for row in range(height):
+	for pixel in range(width):
+		img_enc[row][pixel] = pallier.encrypt(img[row][pixel], ekey)
 
 print "%s encrypted, Decrypting image.." % fname
-dec = None
-decrypt_vec = np.vectorize(pallier.decrypt)
-img_dec = decrypt_vec(img)
+
+img_dec = np.zeros((height, width), dtype=np.uint8)
+for row in range(height):
+	for pixel in range(width): 
+		e = pallier.encrypt(img[row][pixel], ekey)
+		img_dec[row][pixel] = pallier.decrypt(e, dkey, ekey)
 
 print "Image decrypted"
+cv2.imwrite("dec_"+fname, img_dec)
 
 def show_and_wait(title, frame):
 	cv2.imshow(title, frame)
@@ -31,22 +51,25 @@ def show_and_wait(title, frame):
 			break
 
 def show_hist(frame):
-	plt.hist(frame.ravel(), 256, [0, 256])
+	plt.hist(frame.ravel(), 256)
 	plt.show()
 
 def show_lenna():
-	show_and_wait("Gray Lenna", img)
+	show_and_wait("Original", img)
 
 def show_lenna_hist():
 	show_hist(img)
 
-def show_encrypted():
-	show_hist(img_enc)
-	show_and_wait("Encrypted Lenna", img_enc)
-
 def show_decrypted():
+	show_and_wait("Decrypted Image", img_dec)
+
+def show_decrypted_hist():
 	show_hist(img_dec)
-	show_and_wait("Decrypted Lenna", img_dec)
+
+def show_encrypted_hist():
+	show_hist(img_enc)
 
 if __name__ == '__main__':
-	show_encrypted()
+	show_hist(img)
+	show_hist(img_enc)
+	show_decrypted()
--- pallier.py
+++ pallier.py
@@ -1,5 +1,10 @@
 import gmpy2
 from Crypto.Util import number
+from time import time
+
+def L(x, n):
+	return gmpy2.t_div(x-1, n)
+
 def generate_key(n_len):
 	p=gmpy2.mpz(number.getPrime(n_len))
 	q=gmpy2.mpz(number.getPrime(n_len))
@@ -8,19 +13,33 @@ def generate_key(n_len):
 	lam=phin
 	g=n+1
 	m=number.inverse(phin,n)
+
 	return (n,g),(lam,m)
+
+def encrypt(m, key):
+	(n, g) = key
+
+	t = int(time())
+	rand_state = gmpy2.random_state(t)
+	r = gmpy2.mpz_random(rand_state, n-1)+1
 	
-	# lam=gmpy2.lcm(p-1,q-1)
-	# select random integer g
-	# m=number.inverse((L(pow(g,lam,n*n))),n)
+	c = gmpy2.t_mod(g**m * r**n, n**2)
+	return c
 
-	
+def decrypt(c, dkey, ekey):
+	lam, mu = dkey
+	n, g = ekey
+	x = gmpy2.t_mod(c**lam, n**2)
+	return gmpy2.t_mod(L(x, n)*mu, n)
 
-def L(x):
-	pass
+if __name__ == '__main__':
+	message = 103
+	print "Encrypting message: %d" % message
+	ekey, dkey = generate_key(10)
 
-def encrypt():
-	pass
-
-def decrypt():
-	pass
+	e = encrypt(message, ekey)
+	dmsg = decrypt(e, dkey, ekey)
+	if message == dmsg:
+		print "I do believe its working.. good"
+	else:
+		print "ERR: decrypted msg: %d" % dmsg
