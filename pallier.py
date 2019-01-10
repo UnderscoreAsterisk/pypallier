1f3f378ee0e632c787262e821ed7cc68f005425f
Arindaam Roy <alpharoy14@gmail.com>
2019-01-10 20:47:50 +0530

    Key generation function added

--- pallier.py
+++ pallier.py
@@ -1,6 +1,21 @@
 import gmpy2
+from Crypto.Util import number
+def generate_key(n_len):
+	p=gmpy2.mpz(number.getPrime(n_len))
+	q=gmpy2.mpz(number.getPrime(n_len))
+	n=p*q
+	phin=(p-1)*(q-1)
+	lam=phin
+	g=n+1
+	m=number.inverse(phin,n)
+	
+	# lam=gmpy2.lcm(p-1,q-1)
+	# select random integer g
+	# m=number.inverse((L(pow(g,lam,n*n))),n)
 
-def generate_key():
+	return (n,g),(lam,m)
+	
+def L(x):
 	pass
 
 def encrypt():
