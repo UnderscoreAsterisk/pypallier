a8c51c45c9aa39d13f9957952db3a40aad35fdd2
Arindaam Roy <alpharoy14@gmail.com>
2019-01-18 16:58:34 +0530

    added the alternate function

--- paper/functions.py
+++ paper/functions.py
@@ -13,3 +13,9 @@ def green(data):
 	for i in offsets:
 		msg+=chr(data[i][1])
 	return msg
+def alternate(data):
+	offsets=[(6651,0),(121,1),(12,2),(4094,0),(327,1),(20,2),(3583,0),(5624,1),(5,2),(13820,0),(5624,1),(5,2),(7671,0),(121,1),(5624,1),(22,2),(5113,0),(329,1),(42,2),(5624,0),(121,1)]
+	msg=''
+	for i in offsets:
+		msg+=chr(data[i[0]][i[1]])
+	return msg
